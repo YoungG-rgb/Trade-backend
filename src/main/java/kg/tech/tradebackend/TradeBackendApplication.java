a520b6e0529f8ee4e1db13fa74ba6405f3f02618
Zhalaldin Toichubaev <zhtoichubaev@nurtelecom.kg>
2023-03-14 15:48:34 +0600

    initial monolithic app

---
+++ src/main/java/kg/tech/tradebackend/TradeBackendApplication.java
@@ -0,0 +1,13 @@
+package kg.tech.tradebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeBackendApplication.class, args);
+    }
+
+}
