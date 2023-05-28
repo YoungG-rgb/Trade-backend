218962657c6b69b6c4cc91e272d73a9b09e3b783
YoungG-rgb <zhtoichubaev@mail.ru>
2023-05-29 02:30:21 +0600

    rename to cart

--- src/main/java/kg/tech/tradebackend/controllers/ext/CartController.java
+++ src/main/java/kg/tech/tradebackend/controllers/ext/CartController.java
@@ -1,6 +1,5 @@
 package kg.tech.tradebackend.controllers.ext;
 
-import kg.tech.tradebackend.domain.enums.OrderStatus;
 import kg.tech.tradebackend.domain.models.UserModel;
 import kg.tech.tradebackend.services.OrderService;
 import kg.tech.tradebackend.services.UserService;
@@ -11,19 +10,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("OrderApi")
+@Controller("CartApi")
 @RequiredArgsConstructor
-@RequestMapping("/orders")
-public class OrderController {
+@RequestMapping("/cart")
+public class CartController {
     private final UserService userService;
-    private final OrderService orderService;
 
     @GetMapping
     public String getOrders(Model model){
         UserModel user = userService.findByUsername(SecurityUtils.getAuthenticatedUsername());
         model.addAttribute("user", user);
 
-        return "orders";
+        return "cart";
     }
 
 }
